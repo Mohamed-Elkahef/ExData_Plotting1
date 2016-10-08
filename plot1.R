6e56a4de79e79f5cfd92fcc56ec6f37675a73ca4
Mohamed-Elkahef <kashef84@gmail.com>
2016-10-08 21:42:25 +0200

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,17 @@
+### set the Files location 
+## here we set the loaction of the data files 
+
+flocation<-"D:\\"
+
+##pcDs<-read.csv2(sep =";", file = paste(flocation,sep = "","household_power_consumption.txt"),header = TRUE,na.strings = "?",)
+
+
+exds=subset(pcDs,as.Date(Date, "%d/%m/%Y") ==as.Date("1-2-2007", "%d-%m-%Y") | as.Date(Date, "%d/%m/%Y") ==as.Date("2-2-2007", "%d-%m-%Y")  )
+
+
+hist(as.numeric(as.character(exds$Global_active_power)),freq = TRUE,col="red",main = "Global Active Power",xlab = "Global Active Power (Killowatts)")
+
+
+dev.cur()
+dev.copy(png,paste(flocation,sep = "","plot1.png"))
+dev.off()
