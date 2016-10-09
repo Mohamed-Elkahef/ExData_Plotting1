2a63248014d5108f1562fe529cbe79a9e0dbe2c3
Mohamed-Elkahef <kashef84@gmail.com>
2016-10-09 12:57:40 +0200

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -9,11 +9,11 @@ flocation<-"D:\\"
 pcDs<-read.csv2(sep =";", file = paste(flocation,sep = "","household_power_consumption.txt"),header = TRUE,na.strings = "?")
 exds=subset(pcDs,as.Date(Date, "%d/%m/%Y") ==as.Date("1-2-2007", "%d-%m-%Y") | as.Date(Date, "%d/%m/%Y") ==as.Date("2-2-2007", "%d-%m-%Y")  )
 
-xlab=rbind(c(0,'thu'),c(1440,'fri'),c(2860,'sat'))   
+exds$DateTime <- strptime(paste(exds$Date,exds$Time),"%d/%m/%Y %H:%M:%S")
 
 dev.cur()
-plot(as.numeric(as.character(exds$Global_active_power)),ylab = "Global Active Power (kilowatts)",ylim = c(0,8),xlab = "", type = "l",xaxt ='n')
-axis(labels =xlab[,2] ,at=xlab[,1],side = 1, las=2)
+plot(exds$DateTime,as.numeric(as.character(exds$Global_active_power)),ylab = "Global Active Power (kilowatts)",ylim = c(0,8),xlab = "", type = "l")
+
 
 dev.copy(png,paste(flocation,sep = "","plot2.png"))
 dev.off()
--- plot3.R
+++ plot3.R
@@ -9,11 +9,10 @@ flocation<-"D:\\"
 pcDs<-read.csv2(sep =";", file = paste(flocation,sep = "","household_power_consumption.txt"),header = TRUE,na.strings = "?")
 exds=subset(pcDs,as.Date(Date, "%d/%m/%Y") ==as.Date("1-2-2007", "%d-%m-%Y") | as.Date(Date, "%d/%m/%Y") ==as.Date("2-2-2007", "%d-%m-%Y")  )
 
-xlab=rbind(c(0,'thu'),c(1440,'fri'),c(2860,'sat'))   
+exds$DateTime <- strptime(paste(exds$Date,exds$Time),"%d/%m/%Y %H:%M:%S") 
 
 dev.cur()
-plot(as.numeric(as.character(exds$Sub_metering_1)),ylab = "Energy Sub meetering",xlab = "", type = "l",xaxt ='n')
-axis(labels =xlab[,2] ,at=xlab[,1],side = 1, las=2)
+plot(exds$DateTime,as.numeric(as.character(exds$Sub_metering_1)),ylab = "Energy Sub meetering",xlab = "", type = "l")
 lines(as.numeric(as.character(exds$Sub_metering_2)),type = "l",col="red")
 lines(as.numeric(as.character(exds$Sub_metering_3)),type = "l",col="Blue")
 legend("topright",c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
